d048efa2f32d605e88db204cb55e0650f1fe811c
Henriqque <hdoido1992@gmail.com>
2022-09-15 14:11:27 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './style.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+    constructor(props) {
+        super(props)
+        this.state = {
+            timer: 0
+        }
+
+        this.temp = null
+        this.handleTimerStart = this.handleTimerStart.bind(this)
+        this.handleTimerStop = this.handleTimerStop.bind(this)
+    }
+
+    handleTimerStart() {
+        if(this.temp !== null) {
+            clearInterval(this.temp)
+            this.temp = null
+        } else {
+            this.temp = setInterval(() => {
+                let state = this.state
+                state.timer += 0.1
+    
+                this.setState({state})
+            }, (100));
+        }
+    }
+
+    handleTimerStop() {
+        if(this.temp !== null) {
+            clearInterval(this.temp)
+            this.setState({ timer: 0 })
+            this.temp = null
+        } else {
+            this.setState({ timer: 0 })
+        }
+    }
+
+    render(){
+        return(
+            <div className='container'>
+                <img src={require('./assets/cronometro.png')} className='img' />
+                <a className='timer'>
+                    {this.state.timer.toFixed(1)}
+                </a>
+                <div className='areaBtn'>
+                    <a onClick={this.handleTimerStart} className='button'>
+                        { this.temp === null ? 'Começar' : 'Pausar'}
+                    </a>
+                    <a onClick={this.handleTimerStop} className='button'>
+                        ZERAR
+                    </a>
+                </div>
+            </div>
+        );
+    }
 }
-
-export default App;
+export default App
